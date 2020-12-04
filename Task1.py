f161497b7b717703a60f155ab8deeff17954067a
johnnybuildsit <johnnypr@udel.edu>
2020-12-03 21:57:30 -0500

    complete task 1

--- Task1.py
+++ Task1.py
@@ -18,3 +18,23 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+
+def get_unique_numbers(in_list):
+    unique_numbers = []
+    for entry in in_list:
+        calling_num = entry[0]
+        receiving_num = entry[1]
+        if calling_num not in unique_numbers:
+            unique_numbers.append(calling_num)
+        if receiving_num not in unique_numbers:
+            unique_numbers.append(receiving_num)
+
+    return len(unique_numbers)
+
+
+uniqueNumbersTotal = 0
+uniqueNumbersTotal += get_unique_numbers(texts)
+uniqueNumbersTotal += get_unique_numbers(calls)
+
+print("There are {0} different telephone numbers in the records.".format(uniqueNumbersTotal))
