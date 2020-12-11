830c970cecc96e366184fb8a8003149b6f306fb1
johnnybuildsit <johnnypr@udel.edu>
2020-12-11 16:08:39 -0500

    linked list

---
+++ linkedList/LinkedList.py
@@ -0,0 +1,40 @@
+from Node import Node
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def to_list(self):
+        out = []
+        node = self.head
+        while node:
+            out.append(node.value)
+            node = node.next
+        return out
+
+    def prepend(self, value):
+        node = Node(value)
+
+        if self.head is None:
+            self.head = node
+            return
+
+        node.next = self.head
+        self.head = node
+
+    def append(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        node = self.head
+        while node.next:
+            node = node.next
+
+        node.next = new_node
+
+    def search(self, value):
+        pass
---
+++ linkedList/LinkedListTest.py
@@ -0,0 +1,53 @@
+import unittest
+from LinkedList import LinkedList
+
+
+class MyTestCase(unittest.TestCase):
+    def testPrependToEmptyList(self):
+        linked_list = LinkedList()
+        linked_list.prepend(1)
+
+        self.assertEqual(linked_list.to_list(), [1])
+
+    def testPrependToNonEmptyList(self):
+        linked_list = LinkedList()
+        linked_list.prepend(1)
+        linked_list.prepend(2)
+
+        self.assertEqual(linked_list.to_list(), [2, 1])
+
+    def testAppendToEmptyList(self):
+        linked_list = LinkedList()
+        linked_list.append(1)
+
+        self.assertEqual(linked_list.to_list(), [1])
+
+    def testAppendToNonEmptyList(self):
+        linked_list = LinkedList()
+        linked_list.append(1)
+        linked_list.append(2)
+
+        self.assertEqual(linked_list.to_list(), [1, 2])
+
+    def testSearchForAValue(self):
+        linked_list = LinkedList()
+        linked_list.prepend(2)
+        linked_list.prepend(1)
+        linked_list.append(4)
+        linked_list.append(3)
+
+        self.assertEqual(linked_list.search(1), 1)
+        self.assertEqual(linked_list.search(4), 4)
+
+    def testSearchForNonExistingValue(self):
+        linked_list = LinkedList()
+        linked_list.prepend(2)
+        linked_list.prepend(1)
+        linked_list.append(4)
+        linked_list.append(3)
+
+        self.assertEqual(linked_list.search(7), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
