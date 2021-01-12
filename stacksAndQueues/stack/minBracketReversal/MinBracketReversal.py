2b5df8859f99f9fff88027a26320fd3259c4dbed
johnnybuildsit <johnnypr@udel.edu>
2021-01-11 19:20:39 -0500

    min bracket reversal tests

---
+++ stacksAndQueues/stack/minBracketReversal/MinBracketReversal.py
@@ -0,0 +1,5 @@
+class MinBracketReversal:
+
+    @staticmethod
+    def calculate(brackets):
+        pass
---
+++ stacksAndQueues/stack/minBracketReversal/MinBracketReversalTest.py
@@ -0,0 +1,40 @@
+import unittest
+from MinBracketReversal import MinBracketReversal
+
+
+class MinBracketReversalTest(unittest.TestCase):
+
+    def test1(self):
+        brackets = "}}}}"
+
+        reversals = MinBracketReversal.calculate(brackets)
+
+        self.assertEqual(reversals, 2)
+
+    def test2(self):
+        brackets = "}}{{"
+
+        reversals = MinBracketReversal.calculate(brackets)
+
+        self.assertEqual(reversals, 2)
+
+    def test3(self):
+        brackets = "{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}"
+
+        reversals = MinBracketReversal.calculate(brackets)
+
+        self.assertEqual(reversals, 13)
+
+    def test4(self):
+        brackets = "}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{"
+
+        reversals = MinBracketReversal.calculate(brackets)
+
+        self.assertEqual(reversals, 2)
+
+    def test5(self):
+        brackets = "}}{}{}{}{}{}{}{}{}{}{}{}{}{}{}"
+
+        reversals = MinBracketReversal.calculate(brackets)
+
+        self.assertEqual(reversals, 1)
