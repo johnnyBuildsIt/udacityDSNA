d901fde8717a1e2bf0a0f5e6dcd834ae97866836
johnnybuildsit <johnnypr@udel.edu>
2021-01-13 23:34:04 -0500

    is palindrome

--- recursion/Recursion.py
+++ recursion/Recursion.py
@@ -14,3 +14,11 @@ class Recursion:
         if -len(string) == i:
             return string[i]
         return string[i] + self.reverse_string(string, i - 1)
+
+    def is_palindrome(self, string):
+        if len(string) == 1 or len(string) == 0:
+            return True
+        if string[0] == string[-1]:
+            return self.is_palindrome(string[1:-1])
+        else:
+            return False
