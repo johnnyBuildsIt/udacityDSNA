170c1bc223d935c6d045d72b362eaa92b2bbd159
johnnybuildsit <johnnypr@udel.edu>
2021-02-01 14:08:03 -0500

    lru cache implementation

--- project2/problem1/LRU_CacheTest.py
+++ project2/problem1/LRU_CacheTest.py
@@ -4,7 +4,36 @@ import unittest
 
 class LruCacheTest(unittest.TestCase):
 
-    def test1(self):
+    def testSetOneAndGetOne(self):
+        lru_cache = LruCache(5)
+        lru_cache.set(1, 1)
+
+        result = lru_cache.get(1)
+
+        self.assertEqual(result, 1)
+
+    def testSetOneThenTwoShouldOverWriteOne(self):
+        lru_cache = LruCache(1)
+        lru_cache.set(1, 1)
+        lru_cache.set(2, 2)
+
+        result1 = lru_cache.get(1)
+        result2 = lru_cache.get(2)
+
+        self.assertEqual(result1, -1)
+        self.assertEqual(result2, 2)
+
+    def testDontAddExistingElementsAgain(self):
+        lru_cache = LruCache(1)
+        lru_cache.set(1, 1)
+        lru_cache.set(1, 1)
+        lru_cache.set(1, 1)
+
+        size = lru_cache.lruNodes.size
+
+        self.assertEqual(size, 1)
+
+    def testUdacity1(self):
         our_cache = LruCache(5)
         our_cache.set(1, 1)
         our_cache.set(2, 2)
@@ -15,7 +44,7 @@ class LruCacheTest(unittest.TestCase):
         self.assertEqual(2, our_cache.get(2))
         self.assertEqual(-1, our_cache.get(9))
 
-    def test2(self):
+    def testUdacity2(self):
         our_cache = LruCache(5)
         our_cache.set(1, 1)
         our_cache.set(2, 2)
@@ -23,5 +52,7 @@ class LruCacheTest(unittest.TestCase):
         our_cache.set(4, 4)
         our_cache.get(1)
         our_cache.get(2)
+        our_cache.set(5, 5)
+        our_cache.set(6, 6)
 
         self.assertEqual(-1, our_cache.get(3))
--- project2/problem1/LruCache.py
+++ project2/problem1/LruCache.py
@@ -1,15 +1,27 @@
 from pyllist import dllist
 
+
 class LruCache(object):
 
     def __init__(self, capacity):
-        # Initialize class variables
-        pass
+        self.capacity = capacity
+        self.cacheKeys = {}
+        self.lruNodes = dllist()
 
     def get(self, key):
         # Retrieve item from provided key. Return -1 if nonexistent.
-        pass
+        if key in self.cacheKeys:
+            node = self.cacheKeys[key]
+            self.lruNodes.remove(node)
+            self.lruNodes.appendleft(node)
+            return node.value
+        else:
+            return -1
 
     def set(self, key, value):
         # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
-        pass
+        if self.lruNodes.size == self.capacity:
+            self.cacheKeys.pop(self.lruNodes.pop())
+
+        node = self.lruNodes.appendleft(value)
+        self.cacheKeys[key] = node
