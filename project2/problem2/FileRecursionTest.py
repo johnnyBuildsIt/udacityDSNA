7a6584af7b0aec8dd0a9630a9d0612f8e32e7e16
johnnybuildsit <johnnypr@udel.edu>
2021-02-04 08:57:24 -0500

    add tests for find files

--- project2/problem2/FileRecursionTest.py
+++ project2/problem2/FileRecursionTest.py
@@ -4,9 +4,29 @@ import unittest
 
 class FileRecursionTest(unittest.TestCase):
 
-    def test1(self):
+    def testFindCFiles(self):
         finder = FileRecursion()
 
         search_list = finder.find_files(".c", "testdir")
 
-        self.assertEqual(sorted(search_list), sorted(['b.c', 't1.c', 'a.c', 'a.c']))
+        self.assertEqual(sorted(search_list), sorted(['testdir/subdir1/a.c',
+                                                      'testdir/subdir3/subsubdir1/b.c',
+                                                      'testdir/subdir5/a.c',
+                                                      'testdir/t1.c']))
+
+    def testFindHFiles(self):
+        finder = FileRecursion()
+
+        search_list = finder.find_files(".h", "testdir")
+
+        self.assertEqual(sorted(search_list), sorted(['testdir/subdir1/a.h',
+                                                      'testdir/subdir3/subsubdir1/b.h',
+                                                      'testdir/subdir5/a.h',
+                                                      'testdir/t1.h']))
+
+    def testFindGitFiles(self):
+        finder = FileRecursion()
+
+        search_list = finder.find_files(".gitkeep", "testdir")
+
+        self.assertEqual(sorted(search_list), sorted(['testdir/subdir2/.gitkeep', 'testdir/subdir4/.gitkeep']))
